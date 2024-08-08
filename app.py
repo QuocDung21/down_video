fbac688e0c9be99d3b99d4a171700d6e2b81b5fb
quocdung21 <rangvang2357hn@gmail.com>
2024-08-08 15:20:53 +0700

    add file requirements.txt

--- app.py
+++ app.py
@@ -30,34 +30,34 @@
 #         st.error('Please enter a valid YouTube URL.')
 
 
-
 import streamlit as st
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 from pytubefix import Playlist
-import os
 
-st.title('Anh Lý tốn phí còn tôi thì free')
-url = st.text_input('Enter YouTube URL:', '')
+st.title('Tool')
+url = st.text_input('Enter YouTube URL:', 'https://www.youtube.com/watch?v=Y0Ojxw911s8')
+path = st.text_input('Enter path:', 'C:/Users/quocdung/Downloads')
 # download_type = st.selectbox('Select download type:', ['Video', 'MP3', 'Playlists'])
-download_type = st.selectbox('Select download type:', ['Video', 'MP3'])
-if st.button('Phân tích'):
+download_type = st.selectbox('Select download type:', ['Video', 'Mp3'])
+# path = 'C:/Users/quocdung/Downloads'
+if st.button('Tải'):
     if url:
         try:
             yt = YouTube(url, on_progress_callback=on_progress)
             st.write(f"**Title:** {yt.title}")
             if download_type == 'Video':
                 ys = yt.streams.get_highest_resolution()
-                ys.download('C:\Cty\AI\Demo\api\tool',filename='downloaded_video.mp4')
-                with open('downloaded_video.mp4', 'rb') as f:
-                    st.download_button('Download Video', f, file_name='downloaded_video.mp4')
-                st.success('Video complete !')
-            elif download_type == 'MP3':
+                ys.download(path,filename='downloaded_video.mp4')
+                # with open('downloaded_video.mp4', 'rb') as f:
+                #     st.download_button('Download Video', f, file_name='downloaded_video.mp4')
+                st.success('Down Video complete !')
+            elif download_type == 'Mp3':
                 ys = yt.streams.filter(only_audio=True).first()
-                ys.download(filename='downloaded_audio.mp3')
-                with open('downloaded_audio.mp3', 'rb') as f:
-                    st.download_button('Download MP3', f, file_name='downloaded_audio.mp3')
-                st.success('MP3 complete!')
+                ys.download(path,filename='downloaded_audio.mp3')
+                # with open('downloaded_audio.mp3', 'rb') as f:
+                #     st.download_button('Download MP3', f, file_name='downloaded_audio.mp3')
+                st.success('Down MP3 complete!')
             elif download_type == 'Playlists':
                 pl = Playlist(url)
                 for index, video in enumerate(pl.videos):
